f0eaa91dca327ef8d71db1d237cd1813cb7f2db8
zc <87555332+zclc@users.noreply.github.com>
2021-10-28 06:22:08 +0800

    1

--- plugins/zp108_条形码生成器.js
+++ plugins/zp108_条形码生成器.js
@@ -1,21 +1,43 @@
 function onInit({ exc, props, container }) {
     exc('load("https://cdn.jsdelivr.net/npm/jsbarcode@latest/dist/JsBarcode.all.min.js")', {}, () => {
-        container.appendChild(document.createElement("svg"))
-        new JsBarcode(container.firstElementChild, props.value, {})
+        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+        container.appendChild(svg)
+        new JsBarcode(svg, props.text)
     })
 }
 
 $plugin({
     id: "zp108",
     props: [{
-        prop: "value",
+        prop: "text",
         type: "text",
         label: "内容"
-    }, {
-        prop: "width",
-        type: "number",
-        label: "宽度(px)",
-        ph: "默认是128"
     }],
     onInit
 })
+
+
+
+/*
+Option  Default value   Type
+format  "auto" (CODE128)    String
+width   2   Number
+height  100 Number
+displayValue    true    Boolean
+text    undefined   String
+fontOptions ""  String
+font    "monospace" String
+textAlign   "center"    String
+textPosition    "bottom"    String
+textMargin  2   Number
+fontSize    20  Number
+background  "#ffffff"   String (CSS color)
+lineColor   "#000000"   String (CSS color)
+margin  10  Number
+marginTop   undefined   Number
+marginBottom    undefined   Number
+marginLeft  undefined   Number
+marginRight undefined   Number
+valid   function(valid){}   Function
+
+*/
